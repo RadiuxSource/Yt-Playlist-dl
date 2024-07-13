ba302de552072e6f5901d636c4311905741615db
RadiuxSource <157123399+RadiuxSource@users.noreply.github.com>
2024-07-13 22:39:15 +0530

    Update main.py

--- zenova/main.py
+++ zenova/main.py
@@ -1,1 +1,35 @@
+import asyncio
+import importlib
+from pyrogram import idle
+from zenova import zenova
+from zenova.modules import ALL_MODULES
 
+loop = asyncio.get_event_loop()
+
+async def zenova_boot():
+    for all_module in ALL_MODULES:
+        importlib.import_module("zenova.modules." + all_module)
+    print("------------ ZENOVA ------------")
+    print("-----------•••••••-----------")
+    print("--------••••••••••••--------")
+    print("-----•••••••••••••••-----")
+    print("----••••••••••••••••----")
+    print("---•••••••••••••••••---")
+    print("--••••••••••••••••••--")
+    print("-•••••••••••••••••••-")
+    print("••••••••••••••••••••")
+    print("ZENOVA Bot Started Successfully!")
+    print("••••••••••••••••••••")
+    print("-•••••••••••••••••••-")
+    print("--••••••••••••••••••--")
+    print("---•••••••••••••••••---")
+    print("----••••••••••••••••----")
+    print("-----•••••••••••••••-----")
+    print("--------••••••••••••--------")
+    print("-----------•••••••-----------")
+    print("------------ ZENOVA ------------")
+    await idle()
+    print("Caught an unknown error")
+
+if __name__ == "__main__":
+    loop.run_until_complete(zenova_boot())
