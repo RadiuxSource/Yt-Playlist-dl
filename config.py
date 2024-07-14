80d4bae0047a6b02f14f7f02723353eee5ca4fdd
RadiuxSource <157123399+RadiuxSource@users.noreply.github.com>
2024-07-14 10:59:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 import os
 
-class Config:
-    API_HASH = os.getenv("API_HASH", '15478961975051dfc360958e9ea07830')
-    BOT_TOKEN = os.getenv("BOT_TOKEN", '7481260627:AAERIgH99ZziRP-E2GBC_0Sj2BkXgtShCFA')
-    BOT_USERNAME = os.getenv("BOT_USERNAME", 'radiuxspam6bot')
-    MONGO_URI = os.getenv("MONGO_URI", 'mongodb+srv://adityapatel:aditya310708@cluster0.esldbqp.mongodb.net/?retryWrites=true&w=majority')
-    API_ID = int(os.getenv("API_ID", 24006476))
+API_HASH = os.getenv("API_HASH", '15478961975051dfc360958e9ea07830')
+BOT_TOKEN = os.getenv("BOT_TOKEN", '7481260627:AAERIgH99ZziRP-E2GBC_0Sj2BkXgtShCFA')
+BOT_USERNAME = os.getenv("BOT_USERNAME", 'radiuxspam6bot')
+MONGO_URI = os.getenv("MONGO_URI", 'mongodb+srv://adityapatel:aditya310708@cluster0.esldbqp.mongodb.net/?retryWrites=true&w=majority')
+API_ID = int(os.getenv("API_ID", 24006476))
